25a1f654e354d10f06852a8c0f59c48e21e03c5a
Gabe C <gabe@fracturedcode.net>
2022-10-18 00:36:56 -0700

    add foreach range with duck typing

---
+++ Net.FracturedCode.Utilities.Benchmarks/BenchmarkRangeEnumerator.cs
@@ -0,0 +1,42 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Net.FracturedCode.Utilities;
+
+BenchmarkRunner.Run<BenchmarkRangeEnumerator>();
+/*
+|          Method |  Size |         Mean |      Error |     StdDev | Allocated |
+|---------------- |------ |-------------:|-----------:|-----------:|----------:|
+|   NormalForLoop |    10 |     3.140 ns |  0.0397 ns |  0.0372 ns |         - |
+| ExtendedForLoop |    10 |    10.133 ns |  0.0417 ns |  0.0390 ns |         - |
+|   NormalForLoop |  1000 |   253.020 ns |  2.7288 ns |  2.5525 ns |         - |
+| ExtendedForLoop |  1000 |   259.726 ns |  0.7270 ns |  0.5676 ns |         - |
+|   NormalForLoop | 10000 | 2,476.260 ns | 18.2096 ns | 16.1423 ns |         - |
+| ExtendedForLoop | 10000 | 2,490.464 ns | 12.9125 ns | 12.0784 ns |         - |
+ */
+
+[MemoryDiagnoser(false)]
+public class BenchmarkRangeEnumerator
+{
+	[Params(10, 1000, 10000)]
+	public int Size { get; set; }
+
+	[Benchmark]
+	public void NormalForLoop()
+	{
+		for (int i = 0; i < Size; i++)
+		{
+			noop(i);
+		}
+	}
+
+	[Benchmark]
+	public void ExtendedForLoop()
+	{
+		foreach (int i in ..Size)
+		{
+			noop(i);
+		}
+	}
+
+	private static void noop(int x) {}
+}
---
+++ Net.FracturedCode.Utilities/RangeEnumerator.cs
@@ -0,0 +1,28 @@
+﻿namespace Net.FracturedCode.Utilities;
+
+// https://youtu.be/jmmz1cInNow
+public static class RangeExtensions
+{
+	public static CustomIntEnumerator GetEnumerator(this Range range) => new(range);
+}
+
+public ref struct CustomIntEnumerator
+{
+	public int Current { get; private set; }
+	private readonly int _end;
+	public CustomIntEnumerator(Range range)
+	{
+		if (range.End.IsFromEnd || range.Start.IsFromEnd)
+		{
+			throw new NotSupportedException();
+		}
+		Current = range.Start.Value - 1;
+		_end = range.End.Value;
+	}
+
+	public bool MoveNext()
+	{
+		Current++;
+		return Current <= _end;
+	}
+}
