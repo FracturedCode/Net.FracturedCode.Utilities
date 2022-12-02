d4bab82f79275620748ada3fe577770e95e1fc76
Gabe C <gabe@fracturedcode.net>
2022-12-02 16:40:26 -0700

    dotnet pack script and nuget package improvements for license and README.md

---
+++ pack.sh
@@ -0,0 +1,3 @@
+dotnet pack -c Release --include-symbols --include-source Net.FracturedCode.Utilities/Net.FracturedCode.Utilities.csproj
+echo Open Nuget Package Explorer and remove the obj folder
+echo https://github.com/dotnet/sourcelink/issues/572
