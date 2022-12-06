cc79da2dd9b739b7ba9d8f7f45baa34af204dedb
Gabe C <gabe@fracturedcode.net>
2022-12-05 23:51:22 -0700

    Expose default configuration in GenericBenchmark + version bump

--- Net.FracturedCode.Utilities/GenericBenchmark.cs
+++ Net.FracturedCode.Utilities/GenericBenchmark.cs
@@ -8,13 +8,14 @@ public abstract class GenericBenchmark
 	/// <summary>
 	/// Use [Test] in your implementation so dotnet and the text explorer pick it up.
 	/// Don't forget to use Release when you run the tests.
+	/// Easiest implementation is `=> DefaultBench();`
 	/// </summary>
 	public abstract void Benchmark();
 	
-	protected void DefaultBench() =>
-		BenchmarkRunner.Run(
-			GetType(),
-			ManualConfig
-				.Create(DefaultConfig.Instance)
-				.WithOptions(ConfigOptions.DisableLogFile | ConfigOptions.DisableOptimizationsValidator));
+	protected void DefaultBench(ManualConfig? config = null) => BenchmarkRunner.Run(GetType(), config ?? CustomDefaultConfig);
+
+	protected ManualConfig CustomDefaultConfig =>
+		ManualConfig
+			.Create(DefaultConfig.Instance)
+			.WithOptions(ConfigOptions.DisableLogFile | ConfigOptions.DisableOptimizationsValidator);
 }
