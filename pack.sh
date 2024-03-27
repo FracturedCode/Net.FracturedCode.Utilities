d9a1df8065533ccf466837e149bbd4a92b93cd58
Jesse C <jesse@fracturedcode.net>
2024-03-26 17:20:42 -0700

    iterate pack script

--- pack.sh
+++ pack.sh
@@ -4,5 +4,7 @@ echo https://github.com/dotnet/sourcelink/issues/572 I used to remove the obj fo
 echo Inspect to make sure everything looks good, and save
 read -n 1 -p "read the above lines. waiting for input..."
 read -p "Enter API Key (in password manager): " API_KEY
-read -p "Enter the version so I don't have to read the csproj: " $PACKAGE_VERSION
-dotnet nuget push "Net.FracturedCode.Utilities/Net.FracturedCode.Utilities/bin/Release/Net.FracturedCode.Utilities.${PACKAGE_VERSION}.symbols.nupkg" -k $API_KEY
+read -p "Enter the version so I don't have to read the csproj: " PACKAGE_VERSION
+dotnet nuget push "Net.FracturedCode.Utilities/bin/Release/Net.FracturedCode.Utilities.$PACKAGE_VERSION.symbols.nupkg" \
+  -k "$API_KEY" \
+  --source https://api.nuget.org/v3/index.json
